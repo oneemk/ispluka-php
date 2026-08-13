183b655e8e70628106026daf02a18e2ce9679bc3
oneemk <oneem2010@gmail.com>
2026-08-13 06:36:55 +0600

    feat: wire environment and web routes into bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,26 @@
 declare(strict_types=1);
 
 use Ispluka\Core\Application;
+use Ispluka\Core\Environment;
 use Ispluka\Core\Exceptions\Handler;
 use Ispluka\Core\Routing\Router;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+$environmentFile = dirname(__DIR__) . '/.env';
+if (is_file($environmentFile)) {
+    Environment::load($environmentFile);
+}
+
 $router = new Router();
 $exceptionHandler = new Handler();
+
+$webRoutes = dirname(__DIR__) . '/routes/web.php';
+if (is_file($webRoutes)) {
+    $registerRoutes = require $webRoutes;
+    if (is_callable($registerRoutes)) {
+        $registerRoutes($router);
+    }
+}
 
 return new Application($router, $exceptionHandler);
